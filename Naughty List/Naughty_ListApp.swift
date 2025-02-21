9c04e815e1fbf0d3a6d91ceba886ed577a4b3b17
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-20 21:25:15 -0300

    Initial Commit

---
+++ Naughty List/Naughty_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naughty_ListApp.swift
+//  Naughty List
+//
+//  Created by Christian Manzaraz on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Naughty_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
