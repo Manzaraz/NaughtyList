495fb1157b6757cfc79bb7eec433b8b4842780b0
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-27 13:18:56 -0300

    4. In the same file, create an extension of the Child class that includes Mock data with the following properties: firstName lastName naughty smacks notes Bad Bunny true 1 Conejo Malo Draco Malfoy true 5 Watch out for wands Lisa Simpson false 0 Always tries her best Veruca Salt true 3 Keep away from candy Fred Rogers false 0
    Everyone's favorite
    neighbor

--- Naughty List/Child.swift
+++ Naughty List/Child.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftData
 
-
+@MainActor
 @Model
 class Child {
     var firstName = ""
@@ -32,4 +32,53 @@ class Child {
     }
     
     
+}
+
+extension Child {
+    static var preview: ModelContainer {
+        let container = try! ModelContainer(for: Child.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        
+        // Add Mock Data
+        var children: [Child] = [
+            Child(
+                firstName: "Bad",
+                lastName: "Bunny",
+                naughty: true,
+                smacks: 1,
+                notes: "Conejo Malo"
+            ),
+            Child(
+                firstName: "Draco",
+                lastName: "Malfoy",
+                naughty: true,
+                smacks: 5,
+                notes: "Watch out for wands"
+            ),
+            Child(
+                firstName: "Lisa",
+                lastName: "Simpson",
+                naughty: false,
+                smacks: 0,
+                notes: "Always tries her best"
+            ),
+            Child(
+                firstName: "Veruca",
+                lastName: "Salt",
+                naughty: true,
+                smacks: 3,
+                notes: "Keep away from candy"
+            ),
+            Child(
+                firstName: "Fred",
+                lastName: "Rogers",
+                naughty: false,
+                smacks: 0,
+                notes: "Everyone's favorite neighbor"
+            )
+        ]
+            
+        children.forEach { container.mainContext.insert($0) }
+        
+        return container
+    }
 }
